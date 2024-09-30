93f681a432c2d4338d65588022e1b0a6c018f726
Mahalakshmikudipudi <mahalakshmiguthula125>
2024-09-30 21:55:47 +0530

    adding dom relations

---
+++ 06_DOMRelations.js
@@ -0,0 +1,17 @@
+const ul=document.querySelector('.fruits');
+
+//Parent
+ul.parentElement.style.backgroundColor='blue';
+//ul.parentElement.parentElement.style.backgroundColor='pink';
+
+
+//Children=>children[HTML Collection]
+//ul.children[2].style.backgroundColor='pink';
+//ul.lastElementChild.style.backgroundColor='pink';
+//ul.firstElementChild.style.backgroundColor='pink';
+
+
+
+//sibling
+//ul.nextElementSibling.style.backgroundColor='pink';
+ul.previousElementSibling.style.backgroundColor='pink';
