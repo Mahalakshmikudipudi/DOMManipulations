a74ed93067e53d3465a514da191301cede448345
Mahalakshmikudipudi <mahalakshmiguthula125>
2024-09-30 14:26:55 +0530

    first commit

---
+++ 01_ElementBYID.js
@@ -0,0 +1,15 @@
+const header=document.getElementById('header');
+header.style.backgroundColor='blue';
+header.style.color='white';
+header.style.border='3px solid orange';
+
+const mainHeading=document.getElementById('main-heading');
+mainHeading.textContent='Fresh Fruits Shop';
+
+
+const basketHeading=document.getElementById('basket-heading');
+basketHeading.style.fontStyle='italic';
+
+const thanks=document.getElementById('thanks');
+thanks.innerHTML='<p>Thank you for visiting us.</p>';
+thanks.style.fontWeight='bold';
